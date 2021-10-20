78b07ac6efa2ce285bc430623c132160865e0639
Xiang-tao <1689415053@qq.com>
2021-10-20 18:07:24 +0800

    update

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ def readme_file():
 
 setup(
     name="frogpy",  # 需要打包的名字
-    version="1.0.1",  # The initial release version
+    version="1.0.2",  # The initial release version
     description="frogpy: Implement algorithms and solve equations",
+    long_description=readme_file(),
     packages=find_packages(),
     python_requires=">=3.6",
     license="Apache",
     author="Tao Xiang",  # Full name of the author
     url="https://github.com/xiang-tao",
-    author_email="xiangtao@smail.xtu.edu.cn",
-    long_description=readme_file()
+    author_email="xiangtao@smail.xtu.edu.cn"
 )
